34822d2db6dfe46d37349bd0593c6b0b7d5cc6af
EdsonIramMoreno <79424063+EdsonIramMoreno@users.noreply.github.com>
2022-11-30 23:31:03 -0600

    Eliot y Ed merged

--- src/Pages/MainGroupPage/MainGroupPage.jsx
+++ src/Pages/MainGroupPage/MainGroupPage.jsx
@@ -1,13 +1,8 @@
 
 import './MainGroupPage.css'
 import { NavBarMainGroup } from '../../componenetes/NavBarMainGroup/NavBarMainGroup'
-
-<<<<<<< HEAD
-import { Routes,useNavigate,Route } from 'react-router-dom'
-=======
 import { TareasPage } from "../TareasPage/TareasPage";
 import { Routes, Route } from 'react-router-dom'
->>>>>>> 1deaa10c3ded524070d90963f00cd096d9166cba
 import { onSnapshot, collection} from "firebase/firestore";
 import React, { useEffect, useState, useContext} from "react";
 import {db} from "../../firebase"
